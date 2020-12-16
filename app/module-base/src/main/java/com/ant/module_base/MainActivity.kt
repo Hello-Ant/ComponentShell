931a04471279e040f7f963aa135c7436e9c7babf
Ant <ant_lyt@outlook.com>
2020-12-17 00:23:54 +0800

    Initial commit

---
+++ app/module-base/src/main/java/com/ant/module_base/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ant.module_base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
+
+@Route(path = "/base/MainActivity")
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
